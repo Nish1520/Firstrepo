740de1c4247d4949486beb9c6031db3a8c12b113
Nishant Goshke <nishantrajasthan15@gmail.com>
2021-06-12 14:25:12 +0530

    the inital commit

---
+++ header1.h
@@ -0,0 +1,10 @@
+#ifndef HEADER1_H_INCLUDED
+#define HEADER1_H_INCLUDED
+
+//void invertedPattern(int n);
+//void zeronePattern(int n);
+//void rombusPattern(int n);
+//void numTriangle(int n);
+//void palinTriangle(int n);
+void starPattern(int n);
+#endif // HEADER1_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include"header1.h"
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    //invertedPattern(n);
+    // zeronePattern( n);
+    //rombusPattern(n);
+    //numTriangle(n);
+    // palinTriangle(n);
+     starPattern(n);
+
+
+    return 0;
+}
